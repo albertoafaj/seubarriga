a583503ecd8c1d45e45c8cf314bc0ed051ee2166
Alberto Araújo <albertoafaj@hotmail.com>
2022-12-12 10:05:16 -0300

    feat: create logger to bd connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const app = require('express')();
 const consign = require('consign');
+/* const { query } = require('express'); */
 const knex = require('knex');
 const knexfile = require('../knexfile');
 
@@ -17,4 +18,14 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+// eslint-disable-next-line no-shadow
+
+// LOGGER
+
+/* app.db.on('query', (query) => {
+  console.log({ sql: query.sql, bindings: query.bindings ? query.bindings.join(',') : '' });
+})
+.on('query-response', (response) => console.log(response))
+.on('error', (error) => console.log(error));
+ */
 module.exports = app;
