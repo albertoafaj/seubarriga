be06b5d75dfd9c0fad708295ca1041182a4007eb
Alberto Araújo <albertoafaj@hotmail.com>
2022-12-30 10:02:30 -0300

    feat: create log store device

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
+const winston = require('winston');
+const uuid = require('uuidv4');
 const knexfile = require('../knexenvs');
 
-console.log(`Aplication connection in: ${process.env.NODE_ENV}`);
-
 app.db = knex(knexfile[process.env.NODE_ENV]);
+
+app.log = winston.createLogger({
+  level: 'debug',
+  transports: [
+    new winston.transports.Console({ format: winston.format.json({ space: 1 }) }),
+    new winston.transports.File({
+      filename: 'logs/error.log',
+      level: 'warn',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json({ space: 1 })),
+    }),
+  ],
+});
+
+app.log.debug(`Aplication connection in: ${process.env.NODE_ENV}`);
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/passport.js')
@@ -24,8 +38,9 @@ app.use((err, req, res, next) => {
   if (name === 'ValidationError') res.status(400).json({ error: message });
   else if (name === 'WrongResourceError') res.status(403).json({ error: message });
   else {
-    console.log(message);
-    res.status(500).json({ name, message, stack });
+    const id = uuid();
+    app.log.error({ id, name, message, stack });
+    res.status(500).json({ id, error: 'Falha interna' });
   }
   next(err);
 });
